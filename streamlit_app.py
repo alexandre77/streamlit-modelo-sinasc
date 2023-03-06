45885e44877671120aba3c690776bc4a6dece059
Alexandre <xandesilva@gmail.com>
2023-03-05 22:46:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,12 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 
 from sklearn.metrics import auc
+
 #from sklearn.metrics import plot_roc_curve
 #from scikitplot.metrics import plot_roc_curve
-from scikitplot.classifiers import plot_roc_curve_with_cv
+#from scikitplot.classifiers import plot_roc_curve_with_cv
+import scikitplot.plotters as skplt
+
 from sklearn.model_selection import StratifiedKFold
 
 from sklearn.model_selection import train_test_split
@@ -100,36 +103,14 @@ def modelo_roc(dataset, classificador, variavel = [], selecionadas = [], titulo 
         else:
             classifier = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
 
-        tprs = []
-        aucs = []
-        mean_fpr = np.linspace(0, 1, 100)
-
         fig, ax = plt.subplots()
         for i, (train, test) in enumerate(cv.split(X, y)):
-            classifier.fit(X[train], y[train])
-            #viz = plot_roc_curve(classifier, X[test], y[test], name='ROC fold {}'.format(i), alpha=0.3, lw=1, ax=ax)
-            viz = plot_roc_curve_with_cv(classifier, X[test], y[test], title='ROC fold {}'.format(i), ax=ax)
-            #interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-            #interp_tpr[0] = 0.0
-            #tprs.append(interp_tpr)
-            aucs.append(viz.roc_auc)
+            nb = classifier.fit(X[train], y[train])
+            y_probas = nb.predict_proba(X[test])
+            skplt.plot_roc_curve(y[test], y_probas, ax=ax)
 
         ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
                 label='Chance', alpha=.8)
-
-        mean_tpr = np.mean(tprs, axis=0)
-        mean_tpr[-1] = 1.0
-        mean_auc = auc(mean_fpr, mean_tpr)
-        std_auc = np.std(aucs)
-        ax.plot(mean_fpr, mean_tpr, color='b',
-                label=r'Média ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-                lw=2, alpha=.8)
-
-        std_tpr = np.std(tprs, axis=0)
-        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-        ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                        label=r'$\pm$ 1 std. dev.')
 
         ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
                title=titulo)
@@ -176,36 +157,14 @@ def modelo_roc(dataset, classificador, variavel = [], selecionadas = [], titulo 
             else:
                 classifier = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
 
-            tprs = []
-            aucs = []
-            mean_fpr = np.linspace(0, 1, 100)
-
             fig, ax = plt.subplots()
             for i, (train, test) in enumerate(cv.split(X, y)):
-                classifier.fit(X[train], y[train])
-                #viz = plot_roc_curve(classifier, X[test], y[test], name='ROC fold {}'.format(i), alpha=0.3, lw=1, ax=ax)
-                viz = plot_roc_curve_with_cv(classifier, X[test], y[test], title='ROC fold {}'.format(i), ax=ax)
-                #interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-                #interp_tpr[0] = 0.0
-                #tprs.append(interp_tpr)
-                aucs.append(viz.roc_auc)
+                nb = classifier.fit(X[train], y[train])
+                y_probas = nb.predict_proba(X[test])
+                skplt.plot_roc_curve(y[test], y_probas, ax=ax)
 
             ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
                     label='Chance', alpha=.8)
-
-            mean_tpr = np.mean(tprs, axis=0)
-            mean_tpr[-1] = 1.0
-            mean_auc = auc(mean_fpr, mean_tpr)
-            std_auc = np.std(aucs)
-            ax.plot(mean_fpr, mean_tpr, color='b',
-                    label=r'Média ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-                    lw=2, alpha=.8)
-
-            std_tpr = np.std(tprs, axis=0)
-            tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-            tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-            ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                            label=r'$\pm$ 1 std. dev.')
 
             ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
                    title="Predição de mortalidade materna (2020-2021) - %s" %(variavel[ii]))
